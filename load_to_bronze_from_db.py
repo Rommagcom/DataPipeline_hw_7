5dbd36b162332ee9c870488fdc7b78a532f0a067
Roman.iv <roman.ivanitskiy@airastana.com>
2021-08-08 21:05:30 +0600

    Выгрузите данные из dshop PostgreSQL таблиц в RAW(Bronze).

---
+++ load_to_bronze_from_db.py
@@ -0,0 +1,54 @@
+import os
+import yaml
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime
+
+from functions.load_to_bronze_silver import load_to_bronze
+
+default_args = {
+    "owner": "airflow",
+    "email_on_failure": False
+}
+
+
+
+def return_tables():
+    return [
+        {"table":"aisles","output":"ailses.csv"},
+        {"table":"clients","output":"clients.csv"},
+        {"table":"departments","output":"departments.csv"},
+        {"table":"orders","output":"orders.csv"},
+        {"table":"products","output":"products.csv"},
+        {"table":"store_types","output":"store_types.csv"},
+        {"table":"stores","output":"stores.csv"}]
+        
+        
+def load_to_bronze_group(value, for_date):
+    return PythonOperator(
+        task_id="load_"+value+"_to_bronze",
+        python_callable=load_to_bronze,
+        op_kwargs={"table": value},
+        provide_context=True,
+    )
+        
+dag = DAG(
+    dag_id="load_to_bronze_from_db",
+    description="Load data from PostgreSQL dshop database to Data Lake bronze",
+    schedule_interval="@daily",
+    start_date=datetime(2021, 8, 8)
+)
+
+dummy1 = DummyOperator(
+    task_id='start_load_to_bronze',
+    dag=dag
+)
+dummy2 = DummyOperator(
+    task_id='finish_load_to_bronze',
+    dag=dag
+)
+
+for table in return_tables():
+    dummy1 >> load_to_bronze_group(table['table'], datetime.now()) >> dummy2
---
+++ load_to_bronze_silver.py
@@ -0,0 +1,58 @@
+import psycopg2
+import logging
+import os
+from datetime import datetime
+
+from hdfs import InsecureClient
+from airflow.hooks.base_hook import BaseHook
+
+import pyspark
+from pyspark.sql import SparkSession
+
+
+def load_to_bronze(table, **context):
+
+    execution_date = context['execution_date']
+    for_date = execution_date.strftime("%d_%m_%Y")
+    hdfs_conn = BaseHook.get_connection('datalake_hdfs')
+    pg_conn = BaseHook.get_connection('oltp_postgres')
+    pg_creds = {
+        'host': pg_conn.host,
+        'user': pg_conn.login,
+        'password': pg_conn.password,
+        'database': 'dshop_bu'
+    }
+
+    logging.info(f"Writing table {table} for date {for_date} from {pg_conn.host} to Bronze")
+    client = InsecureClient("http://"+hdfs_conn.host, user=hdfs_conn.login)
+    
+    with psycopg2.connect(dbname='dshop_bu', user=pg_conn.login, password=pg_conn.password, host=pg_conn.host) as pg_connection:
+        cursor = pg_connection.cursor()
+        with client.write(os.path.join('new_datalake', 'bronze','dshop',for_date, table)) as csv_file:
+            cursor.copy_expert(f"COPY {table} TO STDOUT WITH HEADER CSV", csv_file)
+    logging.info("Successfully loaded")
+
+
+def load_to_silver_spark(table,**context):
+
+    execution_date = context['execution_date']
+    for_date = execution_date.strftime("%d_%m_%Y")
+    pg_conn = BaseHook.get_connection('oltp_postgres')
+
+    pg_url = f"jdbc:postgresql://{pg_conn.host}:{pg_conn.port}/dshop_bu"
+    pg_properties = {"user": pg_conn.login, "password": pg_conn.password}
+
+    spark = SparkSession.builder \
+        .config('spark.driver.extraClassPath'
+                , '/home/user/shared_folder/postgresql-42.2.23.jar') \
+        .master('local') \
+        .appName('upload_outofstock_api') \
+        .getOrCreate()
+
+    logging.info(f"Writing table {table} for date {for_date} from {pg_conn.host} to Silver")
+    table_df = spark.read.jdbc(pg_url, table=table, properties=pg_properties)
+    table_df.write.parquet(
+        os.path.join('new_datalake', 'silver',for_date, table),
+        mode="overwrite")
+
+    logging.info("Successfully loaded")
